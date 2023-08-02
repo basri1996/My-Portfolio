4dba975ad346216838704f9202a84627e6386558
Zura Japaridze <daviti.basiladze475@law.tsu.edu.ge>
2023-08-02 04:07:15 +0400

    My-Portfolio

--- src/data.tsx
+++ src/data.tsx
@@ -24,7 +24,7 @@ export const data = [
       "The Comment Management System is a web application that allows users to post comments, reply to existing comments, and delete comments as needed. The primary objective of this project is to create a seamless and user-friendly interface that fosters effective communication and engagement between users. Its my favorite project .",
     link: "https://teal-fudge-53bf22.netlify.app/",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/CommentPage",
+    gitHubLink: "https://github1s.com/basri1996/CommentPage",
     responsive: "Yes",
   },
   {
@@ -34,7 +34,7 @@ export const data = [
       "The GitHub Search App is a web application built using React.js that enables users to search for GitHub users and view theirrepositories. This project aims to provide a seamless and efficientway to explore GitHub profiles and repositories, empoweringdevelopers to discover and connect with fellow coders andopen-source projects.",
     link: "https://dashing-bonbon-9d6968.netlify.app/",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/GithubSearch",
+    gitHubLink: "https://github1s.com/basri1996/GithubSearch",
     responsive: "No",
   },
   {
@@ -44,7 +44,7 @@ export const data = [
       "Audiophile is your ultimate premium audio destination,meticulously crafted with React.js. This e-commerce web application invites you to explore a curated selection of top-notch headphones,earphones, and speakers from renowned brands. With a seamless shopping experience, we aim to enhance your passion for exceptional sound quality and design.",
     link: "https://rococo-pie-88e885.netlify.app/",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/Audiophile",
+    gitHubLink: "https://github1s.com/basri1996/Audiophile",
     responsive: "Yes",
   },
   {
@@ -54,7 +54,7 @@ export const data = [
       "Introducing Dice Duel, a thrilling JavaScript project! Set a winning score and face off with a friend. Roll virtual dice, hold or risk rolling again, but beware of rolling ones! Strategize for maximum points. Enjoy this two-player game on desktop and mobile.Get ready for nostalgic fun and exciting gameplay! Let the dice decide your fate!",
     link: "https://effulgent-tulumba-f28ce1.netlify.app/",
     language: "JavaScript",
-    gitHubLink: "https://github.com/basri1996/dice",
+    gitHubLink: "https://github1s.com/basri1996/dice",
     responsive: "No",
   },
   {
@@ -64,7 +64,7 @@ export const data = [
       "ReactJS Form Validation: Develop an intuitive form validation project for efficient data collection. Leverage React's dynamic capabilities to enhance user experience. Implement real-time error checking, ensuring accurate submissions. Streamline the process,saving time and reducing errors. Elevate your web forms with this React-based validation solution!",
     link: "https://eloquent-longma-b7186b.netlify.app/",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/Form-Hook",
+    gitHubLink: "https://github1s.com/basri1996/Form-Hook",
     responsive: "No",
   },
   {
@@ -74,7 +74,7 @@ export const data = [
       "ReactJS Bank Card Creator: Craft a powerful tool enabling users to design custom bank cards. Utilize React components for seamless UI/UX. Offer various card templates, colors, and fonts. Integrate real-time previews for instant feedback. Ensure security measures for safe card generation. Simplify the process of customizing bank cards!",
     link: "https://unique-dodol-3b41e9.netlify.app/",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/Bank-Card",
+    gitHubLink: "https://github1s.com/basri1996/Bank-Card",
     responsive: "No",
   },
   {
@@ -84,7 +84,7 @@ export const data = [
       " The TodoList project in ReactJS is a task management app with add,edit, and delete functions. Users add tasks by entering descriptions in an input field and clicking Add. To edit, users click Edit next to the task, modify the description in an input field, and save it. For deleting tasks, users click the Delete button. The appoffers a user-friendly interface for efficient task organization.",
     link: "https://serene-banoffee-6f5c2f.netlify.app",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/Todo-List",
+    gitHubLink: "https://github1s.com/basri1996/Todo-List",
     responsive: "No",
   },
   {
@@ -94,7 +94,7 @@ export const data = [
       "The TodoList project is a user-friendly task manager application built using HTML, CSS, and JavaScript. It provides essential functionalities such as adding, editing, and deleting tasks, making it an ideal tool for enhanced productivity and organization. Users can easily input task descriptions and create new tasks with a simple click on the 'Add' button.",
     link: "https://profound-kangaroo-ea4377.netlify.app/",
     language: "JavaScript",
-    gitHubLink: "https://github.com/basri1996/Js-TodoList",
+    gitHubLink: "https://github1s.com/basri1996/Js-TodoList",
     responsive: "No",
   },
   {
@@ -104,7 +104,7 @@ export const data = [
       "Savor the irresistible fusion of flavors & innovation as you embark on a visually stunning interface, crafting a culinary odyssey like no other. Prepare your taste buds for a sensational journey,encountering delectable dishes & tantalizing treats! Unleash your inner foodie & immerse yourself in the #OmniFood experience! 🍔🍕🍰 #CSS #HTML #FoodMagic",
     link: "https://wondrous-tarsier-109306.netlify.app/",
     language: "JavaScript",
-    gitHubLink: "https://github.com/basri1996/Omni-Food",
+    gitHubLink: "https://github1s.com/basri1996/Omni-Food",
     responsive: "Yes",
   },
   {
@@ -114,7 +114,7 @@ export const data = [
       "Step into the world of React JS with our captivating Preview Card! Discover the power of this cutting-edge technology as it elegantly displays information in a visually appealing manner. Featuring a sleek and modern design, the Preview Card effortlessly showcases key details.the card's responsive layout ensures seamless viewing on any device.",
     link: "https://eclectic-caramel-4e9510.netlify.app/",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/Preview-Card",
+    gitHubLink: "https://github1s.com/basri1996/Preview-Card",
     responsive: "Yes",
   },
   {
@@ -124,7 +124,7 @@ export const data = [
       "Guess the Number is an exciting JavaScript project that brings the classic guessing game to life in the digital world. With its interactive gameplay and intuitive design, this project challenges users to guess a randomly generated number within a specified range. It's an engaging and enjoyable experience for players of all ages.the project generates a random number.",
     link: "https://monumental-mermaid-802464.netlify.app",
     language: "JavaScript",
-    gitHubLink: "https://github.com/basri1996/Guess-Number",
+    gitHubLink: "https://github1s.com/basri1996/Guess-Number",
     responsive: "No",
   },
   {
@@ -134,7 +134,7 @@ export const data = [
       "JavaScript Form Validation is a crucial technique that ensures the accuracy and completeness of data submitted through web forms. With the power of JavaScript, this feature-rich validation mechanism enhances user experience by providing real-time feedback and preventing erroneous or incomplete data submissions.JavaScript Form Validation performs dynamic validation.",
     link: "https://splendorous-tartufo-47fd88.netlify.app",
     language: "JavaScript",
-    gitHubLink: "https://github.com/basri1996/Form-Validation",
+    gitHubLink: "https://github1s.com/basri1996/Form-Validation",
     responsive: "No",
   },
   {
@@ -144,7 +144,7 @@ export const data = [
       "The JavaScript Calculator is a powerful and versatile web application that empowers users to perform a wide range of mathematical calculations effortlessly. This user-friendly calculator is built entirely in JavaScript and provides a seamless experience for users seeking a reliable tool to solve various equations, arithmetic operations, and more.",
     link: "https://transcendent-druid-c4a7db.netlify.app",
     language: "JavaScript",
-    gitHubLink: "https://github.com/basri1996/Calculator-Js",
+    gitHubLink: "https://github1s.com/basri1996/Calculator-Js",
     responsive: "No",
   },
   {
@@ -154,7 +154,7 @@ export const data = [
       "The ReactJS Tip Calculator is a user-friendly web application designed to streamline the process of calculating tips for services, whether it's at a restaurant, cafe, or any other place where tipping is customary. With its intuitive interface and real-time calculations, this project aims to provide a seamless experience for users while determining appropriate gratuity amounts.",
     link: "https://stellar-beignet-c6df43.netlify.app",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/ReactJs-Calc",
+    gitHubLink: "https://github1s.com/basri1996/ReactJs-Calc",
     responsive: "No",
   },
   {
@@ -164,7 +164,7 @@ export const data = [
       "PhotoSnap is a comprehensive online platform that serves as your ultimate photography hub, catering to both photography enthusiasts and professionals alike. With its sleek and user-friendly interface, PhotoSnap offers a wide array of features and functionalities designed to meet all your photography needs.The heart of PhotoSnap lies within its extensive Gallery",
     link: "https://voluble-crumble-41937d.netlify.app",
     language: "React Js",
-    gitHubLink: "https://github.com/basri1996/PhotoSnap-React",
+    gitHubLink: "https://github1s.com/basri1996/PhotoSnap-React",
     responsive: "Yes",
   },
   {
@@ -174,7 +174,7 @@ export const data = [
       "The Cinema Places Book is a small-scale JavaScript project that aims to simplify the process of booking cinema seats and managing reservations. This web application provides users with an intuitive interface to search for movies, select their preferred cinema locations, and book seats for their desired showtimes.The project centers around enhancing the user experience.",
     link: "https://mellow-palmier-b83cc3.netlify.app",
     language: "JavaScript",
-    gitHubLink: "https://github.com/basri1996/Movie-Stage",
+    gitHubLink: "https://github1s.com/basri1996/Movie-Stage",
     responsive: "No",
   },
   {
